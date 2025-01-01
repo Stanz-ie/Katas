cbbf5f1ad4bb80a00690cefd3524d8eff1c42738
constance <constance.mendez.harscouet@gmail.com>
2025-01-01 15:09:24 +0100

    solution

---
+++ counting_sheep/my_function.py
@@ -0,0 +1,13 @@
+array1 = [True,  True,  True,  False,
+    True,  True,  True,  True ,
+    True,  False, True,  False,
+    True,  False, False, True ,
+    True,  True,  True,  True ,
+    False, False, True,  True ]
+
+def count_sheeps(sheeps):
+    return sheeps.count(True)
+
+# print(count_sheeps(array1))
+
+
